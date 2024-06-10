a7ecca40799139f062eb595770f6eb48707e7d0e
frhandev <frhandev@gmail.com>
2024-06-10 11:07:56 +0300

    First Project added (Vitality-Gym)

--- src/Components/ProjectsSection/ProjectsSection.jsx
+++ src/Components/ProjectsSection/ProjectsSection.jsx
@@ -1,11 +1,18 @@
 import styles from './projectsSection.module.css'
 import ProjectContainer from './ProjectContainer'
+import vitalityImg from './projectAssets/Vitality-Gym.jpeg'
 
 function ProjectsSection() {
     return (
         <section id='projects' className={styles.projectsSection}>
             <h1>my recent Projects</h1>
             <div className={styles.projectCardContainer}>
+            <ProjectContainer 
+                projectImage={vitalityImg} 
+                projectTitle='Vitality Gym'
+                projectDiscreption='A landing page for a fictional fitness gym designed using HTML, CSS, JavaScript, and React.'
+                projectLink='https://vitality-gym.netlify.app/'
+                />
             </div>
         </section>
     );
